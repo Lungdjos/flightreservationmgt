c53d88edc583614b987e490cdb9439b20296d7ab
Lungdjos <lungudjos@gmail.com>
2023-02-28 09:24:56 +0200

    first commit

---
+++ src/main/java/com/lungu/flightreservationmgt/flightreservationmgt/FlightreservationmgtApplication.java
@@ -0,0 +1,13 @@
+package com.lungu.flightreservationmgt.flightreservationmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightreservationmgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightreservationmgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lungu/flightreservationmgt/flightreservationmgt/FlightreservationmgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lungu.flightreservationmgt.flightreservationmgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightreservationmgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
