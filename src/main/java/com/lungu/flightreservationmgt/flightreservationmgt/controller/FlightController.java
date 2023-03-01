f2778de33a7d0ae3a67bf298930637c7f2973a22
Lungdjos <lungudjos@gmail.com>
2023-03-01 11:16:42 +0200

    implementing find flights method in the flight repo and flight controller

--- src/main/java/com/lungu/flightreservationmgt/flightreservationmgt/controller/FlightController.java
+++ src/main/java/com/lungu/flightreservationmgt/flightreservationmgt/controller/FlightController.java
@@ -1,13 +1,16 @@
 package com.lungu.flightreservationmgt.flightreservationmgt.controller;
 
+import com.lungu.flightreservationmgt.flightreservationmgt.entities.Flight;
 import com.lungu.flightreservationmgt.flightreservationmgt.repos.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class FlightController {
@@ -17,8 +20,9 @@ public class FlightController {
 
     @RequestMapping("/availableFlights")
     public String findFlights(@RequestParam("from") String from, @RequestParam("to") String to,
-                              @RequestParam("departureDate") @DateTimeFormat(pattern = "dd-mm-yyyy") Date departureDate){
-        flightRepository.findFlights(from, to, departureDate);
+                              @RequestParam("departureDate") @DateTimeFormat(pattern = "dd-mm-yyyy") Date departureDate, ModelMap modelMap){
+        List<Flight> flights = flightRepository.findFlights(from, to, departureDate);
+        modelMap.addAttribute("flights", flights);
         return "displayFlights";
     }
 }
--- src/main/java/com/lungu/flightreservationmgt/flightreservationmgt/repos/FlightRepository.java
+++ src/main/java/com/lungu/flightreservationmgt/flightreservationmgt/repos/FlightRepository.java
@@ -2,9 +2,15 @@ package com.lungu.flightreservationmgt.flightreservationmgt.repos;
 
 import com.lungu.flightreservationmgt.flightreservationmgt.entities.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
+import java.util.List;
 
 public interface FlightRepository extends JpaRepository<Flight, Integer> {
-    void findFlights(String from, String to, Date departureDate);
+    // using the JPQL query to pull data from the database
+    @Query("from Flight where departureCity = :departureCity, arrivalCity = :arrivalCity, dateOfDeparture = :dateOfDeparture")
+    // mapping the parameters to the names in the query
+    List<Flight> findFlights(@Param("departureCity") String from, @Param("arrivalCity") String to, @Param("dateOfDeparture") Date departureDate);
 }
