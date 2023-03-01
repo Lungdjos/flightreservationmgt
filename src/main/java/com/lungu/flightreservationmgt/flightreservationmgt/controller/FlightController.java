d0c01c30cc82c6ba0311afe29689b2a0507002db
Lungdjos <lungudjos@gmail.com>
2023-03-01 10:44:25 +0200

    adding the flight controller

---
+++ src/main/java/com/lungu/flightreservationmgt/flightreservationmgt/controller/FlightController.java
@@ -0,0 +1,24 @@
+package com.lungu.flightreservationmgt.flightreservationmgt.controller;
+
+import com.lungu.flightreservationmgt.flightreservationmgt.repos.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+
+@Controller
+public class FlightController {
+    // injecting the flight repo
+    @Autowired
+    private FlightRepository flightRepository;
+
+    @RequestMapping("/availableFlights")
+    public String findFlights(@RequestParam("from") String from, @RequestParam("to") String to,
+                              @RequestParam("departureDate") @DateTimeFormat(pattern = "dd-mm-yyyy") Date departureDate){
+        flightRepository.findFlights(from, to, departureDate);
+        return "displayFlights";
+    }
+}
--- src/main/java/com/lungu/flightreservationmgt/flightreservationmgt/repos/FlightRepository.java
+++ src/main/java/com/lungu/flightreservationmgt/flightreservationmgt/repos/FlightRepository.java
@@ -3,5 +3,8 @@ package com.lungu.flightreservationmgt.flightreservationmgt.repos;
 import com.lungu.flightreservationmgt.flightreservationmgt.entities.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+
 public interface FlightRepository extends JpaRepository<Flight, Integer> {
+    void findFlights(String from, String to, Date departureDate);
 }
