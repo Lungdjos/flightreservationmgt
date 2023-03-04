6d6b87c0af6bca1ec04439b10c47af9b38b7f27e
Lungdjos <lungudjos@gmail.com>
2023-03-04 09:00:45 +0200

    creating the integrstion layer to handle RESTful

---
+++ src/main/java/com/lungu/flightreservationmgt/flightreservationmgt/controller/rest/ReservationRestController.java
@@ -0,0 +1,28 @@
+package com.lungu.flightreservationmgt.flightreservationmgt.controller.rest;
+
+import com.lungu.flightreservationmgt.flightreservationmgt.dto.ReservationUpdateRequest;
+import com.lungu.flightreservationmgt.flightreservationmgt.entities.Reservation;
+import com.lungu.flightreservationmgt.flightreservationmgt.repos.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ReservationRestController {
+    @Autowired
+    private ReservationRepository reservationRepository;
+    @RequestMapping("/reservation/{id}")
+    public Reservation findReservation(@PathVariable("id") int id){
+        return reservationRepository.findById(id).orElseThrow();
+    }
+
+    @RequestMapping("/reservations")
+    public Reservation updateReservation(ReservationUpdateRequest reservationUpdateRequest){
+        Reservation reservation = reservationRepository.findById(reservationUpdateRequest.getId()).orElseThrow();
+        reservation.setNumberOfBags(reservationUpdateRequest.getNumberOfBags());
+        reservation.setCheckedIn(reservationUpdateRequest.isCheckedIn());
+        return reservationRepository.save(reservation);
+    }
+}
---
+++ src/main/java/com/lungu/flightreservationmgt/flightreservationmgt/dto/ReservationUpdateRequest.java
@@ -0,0 +1,34 @@
+package com.lungu.flightreservationmgt.flightreservationmgt.dto;
+
+
+
+public class ReservationUpdateRequest {
+    private int id;
+    private boolean checkedIn;
+    private int numberOfBags;
+
+    // getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isCheckedIn() {
+        return checkedIn;
+    }
+
+    public void setCheckedIn(boolean checkedIn) {
+        this.checkedIn = checkedIn;
+    }
+
+    public int getNumberOfBags() {
+        return numberOfBags;
+    }
+
+    public void setNumberOfBags(int numberOfBags) {
+        this.numberOfBags = numberOfBags;
+    }
+}
