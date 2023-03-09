1ffb4307e09e2038fd320af337593f71a6ccfeeb
Lungdjos <lungudjos@gmail.com>
2023-03-09 10:58:28 +0200

    configuring the user logger

--- src/main/java/com/lungu/flightreservationmgt/flightreservationmgt/controller/UserController.java
+++ src/main/java/com/lungu/flightreservationmgt/flightreservationmgt/controller/UserController.java
@@ -2,6 +2,8 @@ package com.lungu.flightreservationmgt.flightreservationmgt.controller;
 
 import com.lungu.flightreservationmgt.flightreservationmgt.entities.User;
 import com.lungu.flightreservationmgt.flightreservationmgt.repos.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -15,6 +17,9 @@ public class UserController {
     // injecting the user repo
     @Autowired
     private UserRepository userRepository;
+
+    // creating the logger instance
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
     // method that handles the registration page
     @RequestMapping("/showRegistration")
@@ -38,6 +43,13 @@ public class UserController {
                         @RequestParam("password") String password,
                         ModelMap modelMap){
         String returnValue; // the return value local variable
+
+        // implementing the logger levels
+        LOGGER.error("ERROR");
+        LOGGER.warn("WARN");
+        LOGGER.info("INFO");    // with SLF4J ends on this level
+        LOGGER.debug("DEBUG");
+        LOGGER.trace("TRACE");
 
         User user = userRepository.findByEmail(email);
         // comparing the user inputs with tha data in the database
