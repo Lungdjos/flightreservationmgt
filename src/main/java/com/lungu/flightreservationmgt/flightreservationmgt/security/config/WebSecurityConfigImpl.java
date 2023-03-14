40f173631e4666b6e52bbfe13b327591667b4442
Lungdjos <lungudjos@gmail.com>
2023-03-14 12:58:12 +0200

    creating a bean password encoder

--- src/main/java/com/lungu/flightreservationmgt/flightreservationmgt/security/config/WebSecurityConfigImpl.java
+++ src/main/java/com/lungu/flightreservationmgt/flightreservationmgt/security/config/WebSecurityConfigImpl.java
@@ -1,15 +1,23 @@
 package com.lungu.flightreservationmgt.flightreservationmgt.security.config;
 
 import com.lungu.flightreservationmgt.flightreservationmgt.security.WebSecurityConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigImpl extends WebSecurityConfiguration implements WebSecurityConfig {
-    // http security configurer
+    // the BCryptEncoder method
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    // http security configurer method
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
         // adding all urls that can be accessed by all users and certain users
